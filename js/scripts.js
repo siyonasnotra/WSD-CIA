d885e8dac19def2a577c46ad9a95dfcf362deb27
Siyona Snotra <141633032+siyonasnotra@users.noreply.github.com>
2024-08-14 12:49:54 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// XHR to fetch data from GitHub
+function loadGitHubData() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.github.com/repos/username/repository/commits", true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var commits = JSON.parse(xhr.responseText);
+            var output = '<ul>';
+            commits.forEach(function(commit) {
+                output += '<li>' + commit.commit.message + ' by ' + commit.commit.author.name + '</li>';
+            });
+            output += '</ul>';
+            document.getElementById('github-data').innerHTML = output;
+        }
+    };
+    xhr.send();
+}
+
+// Call the function when the window loads
+window.onload = loadGitHubData;
